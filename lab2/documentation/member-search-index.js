d4439b5137295061f0831916f614e10dc93517a3
Icariami <gosiamak11@gmail.com>
2024-02-11 12:46:57 +0100

    documentation

---
+++ lab2/documentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Calendar","l":"Calendar()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Calendar","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
